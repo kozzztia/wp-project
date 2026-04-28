fc627e3085f701862235995e5ce801bc9845f7bc
Kostia <kozzztia@gmail.com>
2026-04-28 18:54:30 +0300

    next comkmit v7

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php get_header(); ?>
-<h1>Hello world ! v6.0</h1>
+<h1>Hello world ! v7.0</h1>
 <p>Lorem ipsum dolor sit amet.</p>
+<p>Lorem ipsum dolor sit amet.1</p>
+<p>Lorem ipsum dolor sit amet.2</p>
+<p>Lorem ipsum dolor sit amet.3</p>
 <?php get_footer(); ?>
