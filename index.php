d16f8e83eb446f4f3ef34594d59f680c32e018e6
Kostia <kozzztia@gmail.com>
2026-04-28 19:00:11 +0300

    commit  v9

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<h1>Hello world ! v8.0</h1>
+<h1>Hello world ! v9.0</h1>
 <p>Lorem ipsum dolor sit amet.</p>
 <p>Lorem ipsum dolor sit amet.1</p>
 <p>Lorem ipsum dolor sit amet.2</p>
