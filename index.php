eaf3b164fa5c545db74998e43cf74f74ea143fd8
Kostia <kozzztia@gmail.com>
2026-04-29 17:32:15 +0300

    sceleton and css  styles

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 
     //content
 
-    echo '<section class="custom-block">';
-    echo '<h2>Заголовок блока</h2>';
-    echo '<p>Тут будет твой текст или динамика из WP.</p>';
-    echo '</section>';
+        get_template_part('views/blocks/example');
 }
 
---
+++ views/blocks/example.php
@@ -0,0 +1,9 @@
+
+    <div class="customWrapper example-wrapper">
+        <div class="customBlock example"
+             style="
+                 /*background-image: url('*/<?php //echo get_template_directory_uri() . '/assets/img/example-bg.webp' ?>/*')*/
+                 ">
+            example
+        </div>
+    </div>
