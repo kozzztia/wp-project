97b3787b209c0e51cecce6e524660b2b6e2cda09
Kostia <kozzztia@gmail.com>
2026-04-30 21:45:03 +0300

    test  links v2

---
+++ contacts-page.php
@@ -0,0 +1,17 @@
+<?php
+get_template_part('views/overall/layout');
+function render_content()
+{
+    echo "contacts";
+    //content
+    get_template_part('views/blocks/example', null,
+        [
+            'fields' => [
+                'type' => 'block',
+                'style' => 'color : green',
+                'class' => 'example',
+            ],
+        ]
+    );
+}
+
--- views/overall/header-html.php
+++ views/overall/header-html.php
@@ -3,15 +3,14 @@
         <div class="header-logo">
             logo
         </div>
-
         <div class="header-nav">
-            <?php
-            wp_nav_menu(array(
-                    'theme_location' => 'primary',
-                    'menu_id'        => 'primary-menu',
-                    'menu_class'     => 'nav',
-            ));
-            ?>
+                        <?php
+                        wp_nav_menu(array(
+                                'theme_location' => 'primary',
+                                'menu_id'        => 'primary-menu',
+                                'menu_class'     => 'nav',
+                        ));
+                        ?>
         </div>
     </div>
 </header>
