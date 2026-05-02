da01526b61fd7a9d6c38b018190a2214415816d7
Kostia <kozzztia@gmail.com>
2026-05-02 10:46:45 +0300

    bzckground hero

--- views/blocks/hero.php
+++ views/blocks/hero.php
@@ -1,15 +1,18 @@
 <?php $fields = $args['fields'] ?? null;
     $style = $fields['style'] ?? '';
     $class = $fields['class'] ?? '';
+    $background = get_field('hero_background_image_1') ?? get_template_directory_uri() . '/assets/img/hero.webp';
+
+
 ?>
 
 <?php if (!empty($fields)): ?>
-<div class="customWrapper hero-wrapper" >
+<div class="customWrapper hero-wrapper"  style="background-image: url('<?php echo $background ?>')">
     <div class="customBlock hero <?=$class = $fields['class'] ?? ''?>"
         <?php echo $style ? 'style="'.$style.'"' : ''; ?>
     >
         <div class="hero-inner">
-            example
+            hero
         </div>
     </div>
 </div>
