ceda6c81bd6c774bde6552183108d3891d373151
Kostia <kozzztia@gmail.com>
2026-05-02 17:12:11 +0300

    navigatin   generate  links  and  buttons  event? class  activ  in links | testing v2

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
     <meta name='robots' content='noindex, nofollow' />
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" sizes="32x32">
-    <title><?php echo esc_html( get_the_title() ); ?></title>
+    <title><?php echo esc_html( bloginfo('name') ); ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <?php wp_head(); ?>
