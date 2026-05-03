d0a4329f55380debe2e46fe91d2b7bfdf6c748a4
Kostia <kozzztia@gmail.com>
2026-05-03 13:26:56 +0300

    return static  links

--- views/overall/header-html.php
+++ views/overall/header-html.php
@@ -1,32 +1,61 @@
 <header class="header-wrapper">
     <div class="header">
-
         <div class="header-controls">
             <button type="button" class="header-btn prev">
                 <svg>
-                    <use href=<?php echo get_sprite("arrow-icon")?>></use>
+                    <use href=<?php echo get_sprite("arrow-icon") ?>></use>
                 </svg>
             </button>
             <button type="button" class="header-btn next">
                 <svg>
-                    <use href=<?php echo get_sprite("arrow-icon")?>></use>
+                    <use href=<?php echo get_sprite("arrow-icon") ?>></use>
                 </svg>
             </button>
         </div>
         <nav class="header-menu">
             <ul>
                 <li class="menu-item">
-                    <?php get_template_part('views/overall/logo');?>
+                    <a href="#main">
+                        <svg width="40px" height="40px">
+                            <use href=<?php echo get_sprite("logo-icon") ?>></use>
+                        </svg>
+                    </a>
                 </li>
-
-                <?php
-                wp_nav_menu([
-                        'theme_location' => 'header-menu',
-                        'container'      => false,
-                        'items_wrap'     => '%3$s',
-                        'menu_class'     => '',
-                ]);
-                ?>
+                <li class="menu-item">
+                    <a href="#about">
+                        <svg width="40px" height="40px">
+                            <use href=<?php echo get_sprite("logo-icon") ?>></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#portfolio">
+                        <svg width="40px" height="40px">
+                            <use href=<?php echo get_sprite("logo-icon") ?>></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#study">
+                        <svg width="40px" height="40px">
+                            <use href=<?php echo get_sprite("logo-icon") ?>></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#experience">
+                        <svg width="40px" height="40px">
+                            <use href=<?php echo get_sprite("logo-icon") ?>></use>
+                        </svg>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#contacts">
+                        <svg width="40px" height="40px">
+                            <use href=<?php echo get_sprite("logo-icon") ?>></use>
+                        </svg>
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
