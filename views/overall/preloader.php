e28d9df5b717a53cd908e1e19ba42c470305f2e8
Kostia <kozzztia@gmail.com>
2026-05-16 02:00:52 +0300

    preloader overall start

--- views/overall/preloader.php
+++ views/overall/preloader.php
@@ -5,6 +5,11 @@ $title = $fields['title'] ?? null;
 
 <?php if (!empty($fields)): ?>
 <div class="<?=$class ?>-preloader preloader">
-    preloader
+    <div class="side-front side">front</div>
+    <div class="side-back side">back</div>
+    <div class="side-left side">left</div>
+    <div class="side-right side">right</div>
+    <div class="side-top side">top</div>
+    <div class="side-bottom side">bottom</div>
 </div>
 <?php endif; ?>
