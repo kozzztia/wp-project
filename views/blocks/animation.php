0e3fb6b8ca2b911250109eb02b0f34ee23474d1d
Kostia <kozzztia@gmail.com>
2026-05-17 17:36:59 +0300

    3d

--- views/blocks/animation.php
+++ views/blocks/animation.php
@@ -14,13 +14,6 @@ $image = $fields['image'] ?? null;
 
         <div class="customBlock animation">
             <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
-            <?php get_template_part('views/overall/preloader', null , ['fields' => ['class' => 'animation']]); ?>
 
         </div>
     </section>
